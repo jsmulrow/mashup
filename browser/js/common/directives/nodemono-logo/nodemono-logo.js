53b45aedf6450efd22f8b8658e27da27d3f31be3
Christian Ayscue <coayscue@gmail.com>
2015-09-13 23:40:34 -0400

    Making extension installable

--- browser/js/common/directives/nodemono-logo/nodemono-logo.js
+++ browser/js/common/directives/nodemono-logo/nodemono-logo.js
@@ -1,13 +1,21 @@
-app.directive('nodemonoLogo', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'js/common/directives/nodemono-logo/nodemono-logo.html',
-        link: (scope) => {
+app.directive('nodemonoLogo', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'js/common/directives/nodemono-logo/nodemono-logo.html',
+    link: (scope) => {
 
-          scope.installChromeExt = () => {
-            console.log(`installing Nodemono chrome extension...`)
-          }
+      scope.installChromeExt = () => {
+        console.log(`installing Nodemono chrome extension...`)
+        chrome.webstore.install('https://chrome.google.com/webstore/detail/mihcahmgecmbnbcchbopgniflfhgnkff', () => {
+          alert('Installed!')
+        }, (whyStr) => {
+          console.log(whyStr)
+          alert('Failed to install! Make sure you are using Chrome: https://www.google.com/chrome/browser/desktop/index.html')
+        })
+      }
 
-        }
-    };
+    }
+  };
 });
+
+//sasd
