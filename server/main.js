0a939df8515c4206d282373dfbc3d4a7465d1a2b
Jack Mulrow <jack.mulrow@gmail.com>
2015-12-17 16:52:33 -0500

    disable https server

--- server/main.js
+++ server/main.js
@@ -4,30 +4,29 @@ var chalk = require('chalk');
 // mongoose establishing a connection to a MongoDB database.
 var startDb = require('./db');
 // Create a node server instance! cOoL!
-var server = require('http').createServer();
-var https = require('https');
 var app = require('./app');
 var secureConfig = require('../keys').https;
+var server = require('http').createServer();
+// var secureServer = require('https').createServer(secureConfig);
 var PORT = process.env.PORT || 1337;
 var HTTPS_PORT = 1443;
 //to start HTTPS server run command: npm --server="HTTPS" run-script start
 var startServer = function() {
     //start both http and https server
-    var secureServer = https.createServer(secureConfig, app);
-    secureServer.listen(HTTPS_PORT, function(err) {
-        if (err) console.log(err);
-        console.log('HTTPS server patiently listening on port', HTTPS_PORT);
-    });
-    require('./io')(server); // Attach socket.io.
+    // secureServer.listen(HTTPS_PORT, function(err) {
+    //     if (err) console.log(err);
+    //     console.log(chalk.blue('HTTPS server patiently listening on port', chalk.magenta(HTTPS_PORT)));
+    // });
+    // secureServer.on('request', app);
     server.on('request', app); // Attach the Express application.
     server.listen(PORT, function(err) {
         if (err) console.log(err);
         console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
     });
 };
-startDb.then(startServer).
-catch (function(err) {
-    console.error('Initialization error:', chalk.red(err.message));
-    console.error('Process terminating . . .');
-    process.kill(1);
-});
+startDb.then(startServer)
+    .catch (function(err) {
+        console.error('Initialization error:', chalk.red(err.message));
+        console.error('Process terminating . . .');
+        process.kill(1);
+    });
